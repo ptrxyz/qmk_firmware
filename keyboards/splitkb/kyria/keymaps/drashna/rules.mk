7246b48bc10dd5acc2c07a680d7bf33a520818cc
Drashna Jaelre <drashna@live.com>
2021-10-01 19:11:34 -0700

    [Keyboard] Enable Proton C defaults for SplitKB Kyria (#14490)
    Co-authored-by: Ryan <fauxpark@gmail.com>

--- keyboards/splitkb/kyria/keymaps/drashna/rules.mk
+++ keyboards/splitkb/kyria/keymaps/drashna/rules.mk
@@ -9,11 +9,20 @@ OLED_ENABLE                = yes
 OLED_DRIVER                = SSD1306  # Enables the use of OLED displays
 ENCODER_ENABLE             = yes      # Enables the use of one or more encoders
 
-RGBLIGHT_ENABLE            = no       # Enable keyboard RGB underglow
-RGBLIGHT_STARTUP_ANIMATION = yes
-RGB_MATRIX_ENABLE          = yes
-TAP_DANCE_ENABLE           = yes
-KEY_LOCK_ENABLE            = no
-WPM_ENABLE                 = yes
-SWAP_HANDS_ENABLE          = yes
-LTO_ENABLE                 = no
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = yes      # Audio control and System control
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
+NKRO_ENABLE = yes          # USB Nkey Rollover
+UNICODE_ENABLE = no       # Unicode
+KEY_LOCK_ENABLE = no
+CTPC = yes
+
+# BOOTLOADER = hid
+# BOOTLOADER_SIZE = 512
+# PROGRAM_CMD = 	$(HID_BOOTLOADER_CLI) -mmcu=$(MCU) -w -v $(BUILD_DIR)/$(TARGET).hex
+# TAP_DANCE_ENABLE = yes
+
+WPM_ENABLE = yes
+SWAP_HANDS_ENABLE = yes
